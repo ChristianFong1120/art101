a0cfaa0cb4811914e40e18dd656ccf0be4b916a7
ChristianFong1120 <cfong1111@gmail.com>
2022-04-18 11:18:28 -0700

    lab 4

---
+++ lab4/js/lab.js
@@ -0,0 +1,20 @@
+// Summary.     Pesudocode for cooking a steak.
+// Description. This file contains pesudocode for cooking a steak
+// @link
+// @file        lab4/js/lab4.js
+// @author      Christian Fong, Michael Riley
+// @since       4/18/22
+
+
+//Cooking a steak
+
+//Grab steak from fridge
+//Pat dry steak
+//Oil and season steak
+//Place steak on hot pan
+//While steak is not fully cooked
+//  wait for it to cook for X amount of minutes (cook time dependent on steak thickness)
+//  flip steak
+//baste steak with butter and garlic
+//flip steak and baste otherside
+//let steak rest for 5-10 minutes
