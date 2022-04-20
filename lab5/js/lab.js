4cb924cfed23accea0a32cb8db3a5a7d64904860
ChristianFong1120 <cfong1111@gmail.com>
2022-04-20 11:22:17 -0700

    save progress

---
+++ lab5/js/lab.js
@@ -0,0 +1,23 @@
+// Summary.     Program for printing make and model of car
+// Description. This file contains code for describing Christian's car
+// @link        https://christianfong1120.github.io/art101/lab5/index.html
+// @file        lab4/js/lab5.js
+// @author      Christian Fong, Michael Riley
+// @since       4/20/22
+
+//Base info
+var make = "Suabru";
+var model = "BRZ";
+var year = 2014;
+var color = "Gray";
+var ownIt = true;
+//Calculate year
+var age = 2022 - year;
+
+//printing
+document.writeln("Make: " + make + "<br>");
+document.writeln("Model: " + model + "<br>");
+document.writeln("Color: " + color + "<br>");
+document.writeln("Year: " + year + "<br>");
+document.writeln("Age: " + age + "<br>");
+document.writeln("I own my car: " + ownIt + "<br>");
