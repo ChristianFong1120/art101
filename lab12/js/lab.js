496c6ecbd318828d4f28b4d541b8023640b3655a
ChristianFong1120 <74108356+ChristianFong1120@users.noreply.github.com>
2022-05-16 11:30:56 -0700

    yes

---
+++ lab12/js/lab.js
@@ -0,0 +1,29 @@
+// Summary.     Program for working w/ conditionals
+// Description. This file contains code for working w conditionals
+// @link        https://christianfong1120.github.io/art101/lab12/index.html
+// @file        lab12/js/lab.js
+// @author      Christian Fong, Michael Riley
+// @since       5/16/22
+
+function sortingHat(str){
+  var length = str.length;
+  var rem = length % 4;
+  if(rem == 0){
+    return "Gryffindor";
+  }
+  else if(rem == 1){
+    return "Ravenclaw";
+  }
+  else if(rem == 2){
+    return "Slytherin";
+  }
+  else{
+    return "Hufflepuff";
+  }
+}
+
+
+$("#my-button").click(function(){
+    text = "<p><b>The sorting hat has placed you in " + sortingHat($("input").val()) +  "</b></p>";
+    $("#output").html(text);
+})
